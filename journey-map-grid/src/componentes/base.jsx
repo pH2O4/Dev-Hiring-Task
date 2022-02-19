c80c01ec056a5b9e2d4c87fe5dd91140e03569bc
pH2O4 <ph2oalencar@gmail.com>
2022-02-18 23:58:04 -0300

    --- Building the project base ---

---
+++ journey-map-grid/src/componentes/base.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './base.css'
+import { Component } from "react";
+
+class Base extends Component {
+  render() {
+    return <div className="aside">
+      <div >
+
+      </div>
+      <button>Task</button>
+      <button>Mood board </button>
+      <button>Touchpoints</button>
+      <button>Devices </button>
+      <button>Expectations </button>
+      <button>Emotions</button>
+      <button>Pains </button>
+      <button>Key Messages</button>
+      <button>Sources of Information</button>
+      <button>Keywords </button>
+      <button>Landing Pages</button>
+      <button>KPIs</button>
+    </div>
+
+
+  }
+};
+
+export default Base
---
+++ journey-map-grid/src/main/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './App.css'
+
+import Base from '../componentes/base'
+
+export default props =>
+<div className="App">
+<Base />
+</div>
