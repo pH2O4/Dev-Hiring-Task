7cb719dc2f993c98ae5a6894e2e487d6592ea5ba
pH2O4 <ph2oalencar@gmail.com>
2022-02-22 23:53:07 -0300

    ---configuring hidden content---

---
+++ journey-map-grid/src/componentes/colunas.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './Colunas.css'
+import { Component } from "react";
+
+class Colunas extends Component {
+  render(){
+return <div className="Colunas">
+<button className="AjusteColunas1">COLUNAS</button>
+<button className="AjusteColunas2">COLUNAS</button>
+<button className="AjusteColunas3">COLUNAS</button>
+<button className="AjusteColunas4">COLUNAS</button>
+<button className="AjusteColunas5">COLUNAS</button>
+<button className="AjusteColunas6">COLUNAS</button>
+<button className="AjusteColunas7">COLUNAS</button>
+<button className="AjusteColunas8">COLUNAS</button>
+<div className="Ajuste-conteudo-Coluna1"> KSAKSKAadsfadfaddfsdfadsfSKSAK </div>
+<div className="Ajuste-conteudo-Coluna2"> SAKKSAKAS </div>
+<div className="Ajuste-conteudo-Coluna3"> SAKKSA </div>
+<div className="Ajuste-conteudo-Coluna4">  SASADAS</div>
+<div className="Ajuste-conteudo-Coluna5">DQWDQWD </div>
+<div className="Ajuste-conteudo-Coluna6">DWEDQWD </div>
+<div className="Ajuste-conteudo-Coluna7">DWQDQW </div>
+<div className="Ajuste-conteudo-Coluna8"> DQQWDW</div>
+</div>
+
+  }
+};
+
+export default Colunas
--- journey-map-grid/src/componentes/content.jsx
+++ journey-map-grid/src/componentes/content.jsx
@@ -4,23 +4,38 @@ import "./content.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBorderStyle } from "@fortawesome/free-solid-svg-icons";
 
+import Colunas from "../componentes/colunas"
+
 class Content extends Component{
   render(){
     return <div className="Content"> 
     <div className="ajustennav">
 <button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
+<div className="ConteudoEscondidoY"> <Colunas/> </div>
 <button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
-<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button>
+<div className="ConteudoEscondidoY"></div> 
 <button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
-<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button>
+<div className="ConteudoEscondidoY"></div>
 <button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
-<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button>
-<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button>
-<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button>
+<div className="ConteudoEscondidoY"></div> 
 <button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
-<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button>
-<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button>
-<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button>
+<div className="ConteudoEscondidoY"></div>
+<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
+<div className="ConteudoEscondidoY"></div> 
+<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
+<div className="ConteudoEscondidoY"></div>
+<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
+<div className="ConteudoEscondidoY"></div>
+<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
+<div className="ConteudoEscondidoY"></div>
+<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
+<div className="ConteudoEscondidoY"></div> 
+<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
+<div className="ConteudoEscondidoY"></div>
+<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
+<div className="ConteudoEscondidoY"></div>
+<button className="BotãoConteudo"><FontAwesomeIcon icon={faBorderStyle} />hidden section</button> 
+<div className="ConteudoEscondidoY"></div>
     </div>
  
     </div>
