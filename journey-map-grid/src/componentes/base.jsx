1206ed0b11b0b7f0a4d7d7ed98c32c514e516821
pH2O4 <ph2oalencar@gmail.com>
2022-02-23 12:23:20 -0300

    ---

--- journey-map-grid/src/componentes/base.jsx
+++ journey-map-grid/src/componentes/base.jsx
@@ -1,54 +1,59 @@
 import React from "react";
 import './base.css'
+import './Colunas.css'
 import { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBorderStyle } from "@fortawesome/free-solid-svg-icons";
+import $ from 'jquery';
 
 class Base extends Component {
-  render() {
+
+
+  teste(){
+    console.log("funcionou")
+  }
+
+   
+
+  render() { const teste = teste => this.teste
     return <div className="aside">
 
       <div className="collunsleft">
       <div className="primeiradiv">
           
         </div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /> <span>Storyling</span> </button> 
+      <button id="ButtonsShow1" onClick={teste} ><FontAwesomeIcon icon={faBorderStyle} /> <span>Storyling</span> </button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /> <span>Task</span> </button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /> <span>Task</span> </button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /><span>Mood board</span>  </button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /><span>Mood board</span>  </button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /><span>Touchpoints</span> </button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /><span>Touchpoints</span> </button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /><span>Devices</span>  </button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /><span>Devices</span>  </button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /><span>Expectations</span> </button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /><span>Expectations</span> </button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /><span> Emotions</span></button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /><span> Emotions</span></button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /><span>Pains </span> </button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /><span>Pains </span> </button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /><span>Key Messages</span> </button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /><span>Key Messages</span> </button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /><span> Sources of Information</span></button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /><span> Sources of Information</span></button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /><span>Keywords</span>  </button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /><span>Keywords</span>  </button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /><span> Landing Pages</span></button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /><span> Landing Pages</span></button> 
       <div className="Conteudoescondido"></div>
-      <button><FontAwesomeIcon icon={faBorderStyle} /><span> KPIs</span></button> 
+      <button id="ButtonsShow"><FontAwesomeIcon icon={faBorderStyle} /><span> KPIs</span></button> 
       <div className="Conteudoescondido"></div>
       </div>
-
-   { function EscondendoConteudo() {
-     
-   } 
-      
-    }
     </div>
 
 
   }
-};
+}
+
 
 export default Base
--- journey-map-grid/src/componentes/colunas.jsx
+++ journey-map-grid/src/componentes/colunas.jsx
@@ -5,22 +5,22 @@ import { Component } from "react";
 class Colunas extends Component {
   render(){
 return <div className="Colunas">
-<button className="AjusteColunas1">COLUNAS</button>
-<button className="AjusteColunas2">COLUNAS</button>
-<button className="AjusteColunas3">COLUNAS</button>
-<button className="AjusteColunas4">COLUNAS</button>
-<button className="AjusteColunas5">COLUNAS</button>
-<button className="AjusteColunas6">COLUNAS</button>
-<button className="AjusteColunas7">COLUNAS</button>
-<button className="AjusteColunas8">COLUNAS</button>
-<div className="Ajuste-conteudo-Coluna1"> KSAKSKAadsfadfaddfsdfadsfSKSAK </div>
-<div className="Ajuste-conteudo-Coluna2"> SAKKSAKAS </div>
-<div className="Ajuste-conteudo-Coluna3"> SAKKSA </div>
-<div className="Ajuste-conteudo-Coluna4">  SASADAS</div>
-<div className="Ajuste-conteudo-Coluna5">DQWDQWD </div>
-<div className="Ajuste-conteudo-Coluna6">DWEDQWD </div>
-<div className="Ajuste-conteudo-Coluna7">DWQDQW </div>
-<div className="Ajuste-conteudo-Coluna8"> DQQWDW</div>
+<button id="AjusteColunas1">COLUNAS</button>
+<button id="AjusteColunas2">COLUNAS</button>
+<button id="AjusteColunas3">COLUNAS</button>
+<button id="AjusteColunas4">COLUNAS</button>
+<button id="AjusteColunas5">COLUNAS</button>
+<button id="AjusteColunas6">COLUNAS</button>
+<button id="AjusteColunas7">COLUNAS</button>
+<button id="AjusteColunas8">COLUNAS</button>
+<div id="Ajuste-conteudo-Coluna1"> KSAKSKAadsfadfaddfsdfadsfSKSAK </div>
+<div id="Ajuste-conteudo-Coluna2"> SAKKSAKAS </div>
+<div id="Ajuste-conteudo-Coluna3"> SAKKSA </div>
+<div id="Ajuste-conteudo-Coluna4">  SASADAS</div>
+<div id="Ajuste-conteudo-Coluna5">DQWDQWD </div>
+<div id="Ajuste-conteudo-Coluna6">DWEDQWD </div>
+<div id="Ajuste-conteudo-Coluna7">DWQDQW </div>
+<div id="Ajuste-conteudo-Coluna8"> DQQWDW</div>
 </div>
 
   }
